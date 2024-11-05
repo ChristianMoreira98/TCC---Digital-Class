6b1f8f2d2562c0a607cbbee6e09989438cfd7894
kaike <santanadeabreukaike@gmail.com>
2024-11-04 21:30:31 -0300

    atualização

--- scrpit.js
+++ scrpit.js
@@ -1,1 +1,21 @@
-//
+//função que altera a cor de fundo e do texto para garantue cibtraste.
+
+function trocarCor(){
+    if(document.body.style.backgroundColor==="black") {
+    //se estiver preto, mudar para branco e alterar o texto para preto.
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    
+    //atualiza o texto do botão para indicar o modo escuro
+    document.getElementById("toggleButton").
+    textContent = "Modo Escuro";
+    } 
+    else {
+    document.body.style.backgroundColor = "black"
+    document.body.style.color = "white"
+    //Artualiza o texto do botão para indicar o Modo claro
+    
+    document.getElementById("toggleButton").
+    textContent = "Modo Claro";
+    }
+    }
