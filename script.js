4afec8ff9130d5e88e775652ad4c93aeb9a6b1a2
Matheus lima <matheuslimadasilva0@gmail.com>
2024-11-05 21:41:54 -0300

    2141
    Alteração de botões no cabeçalho...
    Feito por K. Nascimento.

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-//função que altera a cor de fundo e do texto para garantue cibtraste.
+// //função que altera a cor de fundo e do texto para garantue cibtraste.
 
-function trocarCor(){
-    if(document.body.style.backgroundColor==="black") {
-    //se estiver preto, mudar para branco e alterar o texto para preto.
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
+// function trocarCor(){
+//     if(document.body.style.backgroundColor==="black") {
+//     //se estiver preto, mudar para branco e alterar o texto para preto.
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = "black";
     
-    //atualiza o texto do botão para indicar o modo escuro
-    document.getElementById("toggleButton").
-    textContent = "Modo Escuro";
-    } 
-    else {
-    document.body.style.backgroundColor = "black"
-    document.body.style.color = "white"
-    //Artualiza o texto do botão para indicar o Modo claro
+//     //atualiza o texto do botão para indicar o modo escuro
+//     document.getElementById("toggleButton").
+//     textContent = "Modo Escuro";
+//     } 
+//     else {
+//     document.body.style.backgroundColor = "black"
+//     document.body.style.color = "white"
+//     //Artualiza o texto do botão para indicar o Modo claro
     
-    document.getElementById("toggleButton").
-    textContent = "Modo Claro";
-    }
-    }
+//     document.getElementById("toggleButton").
+//     textContent = "Modo Claro";
+//     }
+//     };
+
+    // Rolagem
+ScrollReveal().reveal("#buttonHeader", {
+    distance: "10px",
+    origin: "bottom",
+    duration: 2500,
+});
