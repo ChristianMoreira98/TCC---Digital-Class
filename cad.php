78e89afa7f44edbe8277aee01c3d8803f5ad67fa
ChristianMoreira98 <159092067+ChristianMoreira98@users.noreply.github.com>
2024-11-20 19:08:34 -0300

    update TCC

--- cad.php
+++ cad.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Digital Class | Site Oficial</title>
     <link rel="stylesheet" href="cad.css">
 </head>
 <body>
     <header>
-        <h1>cadastro de clientes | Emporio padaria</h1>
+        <h1>cadastro de clientes | Digital Class</h1>
     </header>
 
     <main class="text">
@@ -16,8 +16,11 @@
         $nome = $_GET["nome"];
         $sobrenome = $_GET["sobrenome"];
         $telefone = $_GET["telefone"];
+        $telefone = $_GET["estado"];
+        $telefone = $_GET["cidade"];
+
         
-        echo "<p> É um prazer te conhecer, o digital class agradece $nome $sobrenome! Entraremos em contato com você!</p>"
+        echo "<p> É um prazer te conhecer, a digital class agradece $nome $sobrenome! Entraremos em contato com você!</p>"
         ?>
  <button class="text-js">
         <a href="javascript:history.go(-1)">Voltar para página anterior</a>
