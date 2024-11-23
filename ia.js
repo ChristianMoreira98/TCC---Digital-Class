9221c70d28ad397c4e97aaa09644ddc3c44604d9
ChristianMoreira98 <159092067+ChristianMoreira98@users.noreply.github.com>
2024-11-23 16:18:19 -0300

    I.A UPDATE

--- ia.js
+++ ia.js
@@ -1,8 +1,16 @@
-// Inicializando o efeito ScrollReveal na primeira seção (#section1)
+// Inicializando o efeito ScrollReveal na primeira seção
 ScrollReveal().reveal('.sessão-1', {
   duration:2000, // Duração do efeito (em milisegundos), define a velocidade da transição
   origin: 'left', // Origem do efeito: o conteúdo virá de baixo pra cima
   distance: '50px', // Diatância que o elemento "viaja" antes de aparecer
   });
+  
+// Inicializando o efeito ScrollReveal na segunda seção 
+ScrollReveal().reveal('.sessão-2', {
+  duration:2000, // Duração do efeito (em milisegundos), define a velocidade da transição
+  origin: 'right', // Origem do efeito: o conteúdo virá de baixo pra cima
+  distance: '50px', // Diatância que o elemento "viaja" antes de aparecer
+  });
+  
 
 
